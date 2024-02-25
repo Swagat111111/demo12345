f59949bd6134b72838c33dc3f21833c779290c2d
sagar <sagar@LAPTOP-HL9EOCAP>
2024-02-25 19:16:43 +0530

    ok

---
+++ src/main/java/Generic/Base_class.java
@@ -0,0 +1,12 @@
+package Generic;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Base_class {
+@Test
+public void testcreatecustomer()
+{
+	Reporter.log("createcustomer" );
+}
+}
